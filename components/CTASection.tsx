1421f70d4911b1df5491f8d4a80c10411205f9b5
Everton Estevao <everton.efdsilva@gmail.com>
2026-02-13 11:40:32 -0300

    remove cod

--- components/CTASection.tsx
+++ components/CTASection.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function CTASection() {
   return (
     <section className="w-full bg-gray-950 text-gray-100 py-26 px-14 text-center">
-      <h2 className="text-3xl font-bold mb-6">Acesse o Artigo e Código</h2>
+      <h2 className="text-3xl font-bold mb-6">Acesse o Artigo</h2>
       <div className="flex flex-col sm:flex-row justify-center gap-4">
         <Button asChild>
           <a
